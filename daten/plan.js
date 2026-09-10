922b9db7fcb0fb2d6d5c4212fbf3978717c2f0a4
Stundenplan-Automatik <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-10 18:17:26 +0000

    Stand vom 10.09.2026 20:17

--- daten/plan.js
+++ daten/plan.js
@@ -23,7 +23,7 @@ const STUNDENPLAN = {
  "nichtBelegteGruppen": [
   "TM+HD"
  ],
- "geprueftAm": "2026-09-10T13:06",
+ "geprueftAm": "2026-09-10T20:17",
  "fensterVon": "2026-09-07T00:00",
  "fensterBis": "2026-11-02T00:00",
  "termine": [
